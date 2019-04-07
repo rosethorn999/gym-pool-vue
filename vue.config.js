e1e29dcd2544d19eb392f40d3108fdfa9e86eda9
rosethorn999 <24700793+rosethorn999@users.noreply.github.com>
2019-04-07 21:23:01 +0800

    Add i18n plugin

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,13 @@
 // vue.config.js
 module.exports = {
-    publicPath: process.env.NODE_ENV === "production" ? "gympool" : "/"
+  publicPath: process.env.NODE_ENV === "production" ? "gympool" : "/",
+
+  pluginOptions: {
+    i18n: {
+      locale: "zh-tw",
+      fallbackLocale: "en",
+      localeDir: "locales",
+      enableInSFC: false
+    }
+  }
 };
